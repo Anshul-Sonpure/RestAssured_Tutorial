1e0affb684177644434be1f33da1cef896bbefa4
Anshul Sonpure <anshul.sonpure@outlook.com>
2022-11-23 15:56:26 +0530

    Fetch any value from json using jsonpath & find

--- src/test/java/testScripts/PrintJsonElement.java
+++ src/test/java/testScripts/PrintJsonElement.java
@@ -1,10 +1,12 @@
 package testScripts;
 
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -35,6 +37,22 @@ public class PrintJsonElement {
         }
         System.out.println(id);
     }
+    /*
+    Another way for doing the same is using find in jsonpath
+    here we have used jsonpath as products.find
+     */
+    @Test
+    public void JsonwithFilters()
+    {
+        Response response = RestAssured.given().get("https://fakestoreapi.com/products");
+        JsonPath path = response.jsonPath();
+//        List<String> prds = path.getList("title");
+//        System.out.println(prds);
+        System.out.println("Id"+path.getString("find{it.title == 'Mens Cotton Jacket'}.id"));
+        Response res= RestAssured.given().get("https://automationexercise.com/api/productsList");
+        JsonPath jsonPath = res.jsonPath();
 
+        System.out.println(jsonPath.getString("products.find{it.name =='Green Side Placket Detail T-Shirt'}.id"));
+    }
 
 }
